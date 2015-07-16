bcfb063e6bb083f145a62bd87bed6b4a3e158f23
unknown <sam_1700@hotmail.com>
2015-07-16 16:44:18 +0200

    Better doc and tests

--- tests/mocha/helpers.js
+++ tests/mocha/helpers.js
@@ -15,8 +15,53 @@ describe('Helper functions', function (){
         assert.equal(ensureObject(empty_object), empty_object);
       });
       it('returns the same object when argument 1 is an object', function (){
-        var filled_object = { a: 'b', c: 'd' }
+        var filled_object = { a: 'b', c: 'd' };
+        assert.equal(ensureObject(filled_object), filled_object);
       });
+      it('returns an empty object when argument 1 is an array', function (){
+        var test = ensureObject([]);
+
+        assert.notEqual(test instanceof Array, true);
+        assert.equal(test instanceof Object, true);
+
+        var keys = 0;
+        for (var key in test) {
+          if (test.hasOwnProperty(key)) {
+            keys++;
+          }
+        }
+        assert.equal(keys, 0);
+      });
+      it('returns an empty object when argument 1 is a string', function (){
+        var test = ensureObject('string!');
+
+        assert.notEqual(test instanceof String, true);
+        assert.equal(test instanceof Object, true);
+
+        var keys = 0;
+        for (var key in test) {
+          if (test.hasOwnProperty(key)) {
+            keys++;
+          }
+        }
+        assert.equal(keys, 0);
+      });
+      it('returns an empty object when argument 1 is a number', function (){
+        var test = ensureObject(5);
+
+        assert.notEqual(test instanceof Number, true);
+        assert.equal(test instanceof Object, true);
+
+        var keys = 0;
+        for (var key in test) {
+          if (test.hasOwnProperty(key)) {
+            keys++;
+          }
+        }
+        assert.equal(keys, 0);
+
+      });
+
     });
 
   });

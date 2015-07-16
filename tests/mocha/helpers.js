6abf72b8d77b255f8a2124fb8b9c5f5d68be637e
unknown <sam_1700@hotmail.com>
2015-07-16 16:26:07 +0200

    Better testing, .codeclimate.yml now exluding tests/*

--- tests/mocha/helpers.js
+++ tests/mocha/helpers.js
@@ -4,10 +4,20 @@ describe('Helper functions', function (){
   describe('#ensureObject', function (){
     var ensureObject = require('./../../lib/helpers/ensureObject.js');
 
-    describe('Requiring the module', function (){
+    describe('##Requiring the module', function (){
       it('returns a function', function (){
         assert.equal(typeof ensureObject, 'function');
       });
     });
+    describe('##Using the function', function (){
+      it('returns the same empty object when argument 1 is an empty object', function (){
+        var empty_object = {};
+        assert.equal(ensureObject(empty_object), empty_object);
+      });
+      it('returns the same object when argument 1 is an object', function (){
+        var filled_object = { a: 'b', c: 'd' }
+      });
+    });
+
   });
 });
